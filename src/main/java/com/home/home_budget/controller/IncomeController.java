e48c1c0a4e9d390cdf736f68efbd9726c72c62bf
kaarelkattai <kaarel.kattai@hotmail.com>
2025-02-05 22:46:57 +0200

    working backend

--- src/main/java/com/home/home_budget/controller/IncomeController.java
+++ src/main/java/com/home/home_budget/controller/IncomeController.java
@@ -1,9 +1,7 @@
 package com.home.home_budget.controller;
 
 import com.home.home_budget.Model.Income;
-import com.home.home_budget.service.CategoryService;
 import com.home.home_budget.service.IncomeService;
-import com.home.home_budget.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
